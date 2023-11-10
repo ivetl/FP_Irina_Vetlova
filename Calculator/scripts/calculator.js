2ebc3c985f1fce18275446c643df045890826986
Irina Vetlova <irinka2006.82@mail.ru>
2023-11-10 13:35:13 +0400

    добавлены замыкания (объект calculatorObject)

--- Calculator/scripts/calculator.js
+++ Calculator/scripts/calculator.js
@@ -9,33 +9,6 @@ const Operations = {
     EQ: "="
 };
 
-var prevValue = "";
-var curOperation = Operations.NONE;
-var curValue = "0"
-var valueChanged = false;
-
-let updateView = () => {
-    
-    document.calc.expr.value = prevValue + curOperation;
-    document.calc.txt.value = curValue;
-    document.getElementById("clr").textContent = curValue == "0" ? "C" : "CE";
-}
-
-function onDigit(digit) {
-    if (!valueChanged || curValue == "0")
-        curValue =  digit != '.' ? digit : "0.";
-    else
-        curValue += digit;
-
-    if (curOperation == Operations.EQ) {
-        curOperation = Operations.NONE;
-        prevValue = "";
-    }
-
-    valueChanged = true;
-    updateView();
-}
-
 let unaryOp = (a, op) => {
     var result = "";
 
@@ -60,68 +33,117 @@ let binaryOp = (a, op, b) => {
     return result + "";
 }
 
-let onOperation = function(operation) {
-    switch (operation) {
-    case Operations.EQ:
-        if (curOperation != Operations.NONE && curOperation != Operations.EQ) {
-            var newValue = binaryOp(prevValue, curOperation, curValue);
-            prevValue += curOperation + curValue;
-            curOperation = operation;
-            curValue = newValue;
-            valueChanged = false;
-        }
-        break;
-    case Operations.SQRT: 
-        prevValue = operation + "(" + curValue + ")";
-        curValue = unaryOp(curValue, operation);
-        curOperation = Operations.EQ;
-        valueChanged = false;
-        break;
-    case Operations.NONE: break;
-    default:
-        if (valueChanged && curOperation != Operations.NONE) {
-            curValue = binaryOp(prevValue, curOperation, curValue);
-            prevValue = curValue;
-            valueChanged = false;
-        }
-        else if (curOperation == Operations.NONE || curOperation == Operations.EQ) {
-            prevValue = curValue;
-            valueChanged = false;
-        }
+var makeCalculator = function () {
+    var prevValue = "";
+    var curOperation = Operations.NONE;
+    var curValue = "0"
+    var valueChanged = false;
 
-        curOperation = operation;
+    function updateView() {
+        
+        document.calc.expr.value = prevValue + curOperation;
+        document.calc.txt.value = curValue;
+        document.getElementById("clr").textContent = curValue == "0" ? "C" : "CE";
     }
-    updateView();
+
+    return {
+        processDigit: function(digit) {
+            if (!valueChanged || curValue == "0")
+                curValue =  digit != '.' ? digit : "0.";
+            else
+                curValue += digit;
+        
+            if (curOperation == Operations.EQ) {
+                curOperation = Operations.NONE;
+                prevValue = "";
+            }
+        
+            valueChanged = true;
+            updateView();        
+        }, 
+        processOperation: function(operation) {
+            switch (operation) {
+            case Operations.EQ:
+                if (curOperation != Operations.NONE && curOperation != Operations.EQ) {
+                    var newValue = binaryOp(prevValue, curOperation, curValue);
+                    prevValue += curOperation + curValue;
+                    curOperation = operation;
+                    curValue = newValue;
+                    valueChanged = false;
+                }
+                break;
+            case Operations.SQRT: 
+                prevValue = operation + "(" + curValue + ")";
+                curValue = unaryOp(curValue, operation);
+                curOperation = Operations.EQ;
+                valueChanged = false;
+                break;
+            case Operations.NONE: break;
+            default:
+                if (valueChanged && curOperation != Operations.NONE) {
+                    curValue = binaryOp(prevValue, curOperation, curValue);
+                    prevValue = curValue;
+                    valueChanged = false;
+                }
+                else if (curOperation == Operations.NONE || curOperation == Operations.EQ) {
+                    prevValue = curValue;
+                    valueChanged = false;
+                }
+        
+                curOperation = operation;
+            }
+            updateView();
+        },
+        processBackspace: function() {
+            curValue = curValue.slice(0, -1);
+
+            if (curValue == "")
+                curValue = "0"
+
+            if (curOperation == Operations.EQ) {
+                prevValue = "";
+                curOperation = Operations.NONE;
+            }
+            
+            valueChanged = true;
+            updateView();
+        },
+        processDecimal: function() {
+            if (curValue.indexOf(".") < 0 || !valueChanged) {
+                onDigit('.');
+            }
+        },
+        processClear: function() {
+            if (curOperation == Operations.EQ || curValue == "0") {
+                prevValue = "";
+                curOperation = Operations.NONE;
+                valueChanged = false;
+            }
+        
+            curValue = "0";
+            updateView();
+        }
+    }
+};
+
+var calculatorObject = makeCalculator();
+
+let onDigit = digit => {
+    calculatorObject.processDigit(digit);
+}
+
+let onOperation = operation => {
+    calculatorObject.processOperation(operation);
 }
 
 let onBackspace = () => {
-    curValue = curValue.slice(0, -1);
-
-    if (curValue == "")
-        curValue = "0"
-
-    if (curOperation == Operations.EQ) {
-        prevValue = "";
-        curOperation = Operations.NONE;
-    }
-    
-    valueChanged = true;
-    updateView();
+    calculatorObject.processBackspace();
 }
 
 let onDecimal = () => {
-    if (curValue.indexOf(".") < 0 || !valueChanged) {
-        onDigit('.');
-    }
+    calculatorObject.processDecimal();
 }
 
 let onClear = () => {
-    if (curOperation == Operations.EQ || curValue == "0") {
-        prevValue = "";
-        curOperation = Operations.NONE;
-        valueChanged = false;
-    }
-
-    curValue = "0";
-    updateView();
+    calculatorObject.processClear();
 }
